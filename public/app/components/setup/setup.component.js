1b588d3db68c5691fe1cfc6aad4c85c9c3adc39c
Nelo Pauselli <nelopauselli@gmail.com>
2018-08-05 20:13:19 -0300

    mejoras en setup

--- public/app/components/setup/setup.component.js
+++ public/app/components/setup/setup.component.js
@@ -11,7 +11,6 @@ function SetupController($scope, $mdDialog, $http) {
 
 	$scope.openAddDevice = function (ev) {
 		$scope.settings = {
-			connected: false,
 			ssid: 'Positano', password: '',
 			mqttConnectionString: 'mqtt://@192.168.1.10:1883',
 			mqttTopicBase: '/casa',
@@ -19,42 +18,46 @@ function SetupController($scope, $mdDialog, $http) {
 			serverAddress: '192.168.1.10',
 			serverPort: 80
 		};
+		$scope.searching = false;
+		$scope.connected = false,
 
-		$mdDialog.show({
-			contentElement: '#newDevicePopup',
-			parent: angular.element(document.body),
-			targetEvent: ev,
-			clickOutsideToClose: true
-		}).then(function () {
-			var url = 'http://192.168.4.1/';
+			$mdDialog.show({
+				contentElement: '#newDevicePopup',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose: false
+			}).then(function () {
+				var url = 'http://192.168.4.1/';
 
-			$http({
-				method: 'POST',
-				url: url,
-				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-				transformRequest: function (obj) {
-					var str = [];
-					for (var p in obj)
-						str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-					return str.join("&");
-				},
-				data: $scope.settings
-			}).then(function (response) {
-				$scope.message="Dispositivo agregado";
-			}, function (error) {
-				$scope.settings.message = error.toString();
+				$http({
+					method: 'POST',
+					url: url,
+					headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+					transformRequest: function (obj) {
+						var str = [];
+						for (var p in obj)
+							str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+						return str.join("&");
+					},
+					data: $scope.settings
+				}).then(function (response) {
+					$scope.message = "Dispositivo agregado";
+				}, function (error) {
+					$scope.settings.message = error.toString();
+				});
+			}, function () {
+
 			});
-		}, function(){
-			
-		});
 	}
 
 	$scope.searchDevice = function () {
+		$scope.searching = true;
 		var loop = setInterval(function () {
 			$http.get('http://192.168.4.1/')
 				.then(function (response) {
 					if (response.data === "I'm a IoT Device") {
-						$scope.settings.connected = true;
+						$scope.searching = false;
+						$scope.connected = true;
 						clearInterval(loop);
 					}
 				}, function (error) {
