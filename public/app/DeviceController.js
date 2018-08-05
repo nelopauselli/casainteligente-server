12a33d893ef7fc44979ed54ba61dd217e10d2251
Nelo Pauselli <nelopauselli@gmail.com>
2018-08-05 02:22:08 -0300

    Detectando y configurando nuevos dispositivos

--- public/app/DeviceController.js
+++ public/app/DeviceController.js
@@ -1,9 +1,9 @@
 angular.module("app")
     .controller('DeviceController', DeviceController);
 
-DeviceController.$inject = ['$scope', '$mdSidenav', 'deviceService', 'socket'];
+DeviceController.$inject = ['$scope', '$mdSidenav', '$mdDialog', '$http', 'deviceService', 'socket'];
 
-function DeviceController($scope, $mdSidenav, deviceService, socket) {
+function DeviceController($scope, $mdSidenav, $mdDialog, $http, deviceService, socket) {
     $scope.devices = deviceService.query(function () {
         $scope.current = $scope.devices[0];
     });
@@ -20,6 +20,59 @@ function DeviceController($scope, $mdSidenav, deviceService, socket) {
     $scope.select = select;
     $scope.toggleMenu = toggleMenu;
 
+    $scope.openAddDevice = function (ev) {
+        $scope.newDevice = {
+            connected: false,
+            ssid: 'Positano', password: '',
+            mqttConnectionString: 'mqtt://@192.168.1.10:1883', 
+            mqttTopicBase: '/casa',
+            deviceName: '',
+            serverAddress: '192.168.1.10',
+            serverPort: '80'
+        };
+
+        $mdDialog.show({
+            contentElement: '#newDevicePopup',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        });
+
+        var loop = setInterval(function () {
+            $http.get('http://192.168.4.1/')
+                .then(function (response) {
+                    if (response.data === "I'm a IoT Device") {
+                        $scope.newDevice.connected = true;
+                        clearInterval(loop);
+                    }
+                }, function (error) {
+                    console.log(error);
+                });
+        }, 1000);
+    }
+
+    $scope.setNewDevice = function (device) {
+        var url = 'http://192.168.4.1/';
+
+        $http({
+            method: 'POST',
+            url: url,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            transformRequest: function (obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: device
+        }).then(function (response) {
+            $scope.newDevice.connected = false;
+            $scope.newDevice.message = "dispositivo agregado";
+        }, function (error) {
+            $scope.newDevice.message = error.toString();
+        });
+    }
+
     function select(device) {
         $scope.current = device;
     }
@@ -27,4 +80,6 @@ function DeviceController($scope, $mdSidenav, deviceService, socket) {
     function toggleMenu() {
         $mdSidenav("left").toggle();
     };
+
+
 }
